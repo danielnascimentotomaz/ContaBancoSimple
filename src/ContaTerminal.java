95a51b23358f2ac04c01a946481ca952591f01c0
danielnascimentotomaz <danielsabio1000@hotmail.com>
2022-11-16 16:23:14 -0300

    projeto final

---
+++ src/ContaTerminal.java
@@ -0,0 +1,62 @@
+/*
+Sintaxe - Desafio
+
+
+Crie o projeto ContaBanco que receberá dados via terminal contendo as características de conta em banco conforme atributos abaixo:
+Dentro do projeto, crie a classe ContaTerminal.java para realizar toda a codificação do nosso programa.
+Revise sobre regras de declaração de variáveis.
+
+Atributo	Tipo	Exemplo
+Numero	Inteiro	1021
+Agencia	Texto	067-8
+Nome Cliente	Texto	MARIO ANDRADE
+Saldo	Decimal	237.48
+
+Revise sobre terminal, main args e a classe Scanner
+Permita que os dados sejam inseridos via terminal sendo que o usuário receberá a mensagem de qual informação será solicitada, exemplo:
+
+Programa: "Por favor, digite o número da Agência !"
+Usuário: 1021 (depois ENTER para o próximo campo)
+Revise sobre concatenação e classe String com método concat
+
+Depois de todas as informações terem sido inseridas, o sistema deverá exibir a seguinte mensagem:
+"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está
+disponível para saque".
+
+Os campos em [ ] devem ser alterados pelas informações que forem inseridas pelos usuários.
+
+
+*/
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args){
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = teclado.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !: ");
+        agencia = teclado.next();
+
+        System.out.println("Por favor, digite seu nome!: ");
+        nomeCliente =  teclado.next();
+
+        System.out.println("Por favor, digite seu saldo!: ");
+        saldo = teclado.nextFloat();
+
+        System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco,\n" +
+                "sua agência é " + agencia +", numero da conta " + numeroConta + " e seu saldo \n" +
+                "é " + saldo + " reais já está disponível para saque");
+
+
+
+    }
+}
